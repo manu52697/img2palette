0920b4d419f39c85571008f0a4341812ab9b8ff2
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-03-02 17:58:30 +0100

    Add basic color extractor

---
+++ src/main/java/com/example/img2palette/util/ColorArray.java
@@ -0,0 +1,33 @@
+package com.example.img2palette.util;
+
+import java.awt.image.BufferedImage;
+import java.util.Arrays;
+
+public class ColorArray {
+
+    private final int[] colors;
+
+    public ColorArray(int[] colors) {
+        this.colors = colors;
+    }
+
+    public ColorArray(BufferedImage image) {
+        this.colors = extractColors(image);
+    }
+
+    public static int[] extractColors(BufferedImage image) {
+        int width, height;
+        width = image.getWidth();
+        height = image.getHeight();
+
+        int[] colors = new int[width * height];
+
+        for (int i = 0; i < width; i++){
+            for (int j = 0; j < height; j++){
+                colors[i*width+j] = image.getRGB(i, j);
+            }
+        }
+        Arrays.sort(colors);
+        return colors;
+    }
+}
