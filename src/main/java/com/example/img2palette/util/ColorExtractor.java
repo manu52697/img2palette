e4e29d7d76c785eb4ee222005ffd41c7b6f4b344
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-03-03 12:08:24 +0100

    Changes to ColorExtrator to return an array of Color instead of an array of int

--- src/main/java/com/example/img2palette/util/ColorExtractor.java
+++ src/main/java/com/example/img2palette/util/ColorExtractor.java
@@ -1,13 +1,14 @@
 package com.example.img2palette.util;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.Arrays;
 
 public class ColorExtractor {
 
-    private final int[] colors;
+    private final Color[] colors;
 
-    public ColorExtractor(int[] colors) {
+    public ColorExtractor(Color[] colors) {
         this.colors = colors;
     }
 
@@ -15,23 +16,28 @@ public class ColorExtractor {
         this.colors = extractColors(image);
     }
 
-    public int[] getColors(){
+    public Color[] getColors(){
         return this.colors;
     }
 
-    public static int[] extractColors(BufferedImage image) {
+    /**
+     * This method reads the color data from a BufferedImage and returns an array stacking the pixel data by column
+     * @param image An BufferedImage instance
+     * @return An array of color values
+     */
+    public static Color[] extractColors(BufferedImage image) {
         int width, height;
         width = image.getWidth();
         height = image.getHeight();
 
-        int[] colors = new int[width * height];
+        Color[] colors = new Color[width * height];
 
         for (int i = 0; i < width; i++){
             for (int j = 0; j < height; j++){
-                colors[i*width+j] = image.getRGB(i, j);
+                colors[i*width+j] = new Color(image.getRGB(i, j));
             }
         }
-        Arrays.sort(colors);
+
         return colors;
     }
 }
