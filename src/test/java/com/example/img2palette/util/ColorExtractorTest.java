43e2f75c9cfec91d32ba3aa2423c939acaf07611
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-03-03 12:57:44 +0100

    Fix tests for ColorExtractor

---
+++ src/test/java/com/example/img2palette/util/ColorExtractorTest.java
@@ -0,0 +1,38 @@
+package com.example.img2palette.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColorExtractorTest {
+
+
+    @Test
+    void extractColors() {
+        int testHeight, testWidth;
+        testWidth = 3;
+        testHeight = 4;
+
+        BufferedImage testImage = new BufferedImage(testWidth, testHeight, BufferedImage.TYPE_INT_RGB);
+        for (int i = 0; i < testWidth; i++) {
+            for (int j = 0; j < testHeight; j++) {
+                testImage.setRGB(i, j, new Color(i*20,j*20,0).getRGB());
+            }
+        }
+
+        ColorExtractor extractor = new ColorExtractor(testImage);
+        Color[] colors = extractor.getColors();
+
+        assertAll("Check color extraction",
+                () -> assertEquals(new Color(0), colors[0]),
+                () -> assertEquals(new Color(0,20,0), colors[1]),
+                () -> assertEquals(new Color(0,40,0), colors[2]),
+                () -> assertEquals(new Color(20,0,0), colors[3]),
+                () -> assertEquals(new Color(20,20,0), colors[4])
+    );
+
+    }
+}
