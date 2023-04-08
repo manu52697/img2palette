a22b2334c10273c31243fbf829d83ee1a1be8b8a
Manuel Artal <32219329+manu52697@users.noreply.github.com>
2023-04-08 11:09:14 +0200

    Add test for update method

--- src/test/java/com/example/img2palette/util/ClosedRangeTest.java
+++ src/test/java/com/example/img2palette/util/ClosedRangeTest.java
@@ -44,4 +44,35 @@ class ClosedRangeTest {
                 ()-> assertEquals(10, range2.getRange())
         );
     }
+
+    @Test
+    void updateRange(){
+
+        // setup
+        ClosedRange emptyRange = new ClosedRange(0,0).updateRange(0);
+        ClosedRange range1 = new ClosedRange(2,3).updateRange(4);
+        ClosedRange range2 = new ClosedRange(3,4).updateRange(2);
+
+        //assertions
+        assertAll("Check updating range; Case: range contains int k",
+                ()-> assertEquals(emptyRange.getRange(),  0),
+                ()-> assertEquals(emptyRange.getMin(), 0),
+                () -> assertEquals(emptyRange.getMax(), 0)
+        );
+
+        assertAll("Check updating range; Case: range not contains int k, k > range.getMax()",
+                ()-> assertEquals(range1.getRange(), 2),
+                ()-> assertEquals(range1.getMin(), 2),
+                () -> assertEquals(range1.getMax(), 4)
+                );
+
+        assertAll("Check updating range; Case: range not contains int k, k < range.getMin()",
+                () -> assertEquals(range2.getRange(), 2),
+                ()-> assertEquals(range2.getMin(),2),
+                () -> assertEquals(range2.getMax(), 4)
+                );
+
+        assertEquals(range1.getRange(), range2.getRange());
+
+    }
 }
